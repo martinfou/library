9e65f503da0ef04376821a548f4b46b0953894fb
martinfou <martinfou@gmail.com>
2016-09-11 18:12:40 -0400

    adding inject javascript

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,10 @@ gulp.task('style',function(){
 
 gulp.task('inject',function(){
     var wiredep = require('wiredep').stream;
-
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib'
+    }
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
         .pipe(gulp.dest('./src/views'));
