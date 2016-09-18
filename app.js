8fec21a51d7eb9c6069a820940a06bc4c904530c
martinfou <martinfou@gmail.com>
2016-09-18 01:32:30 -0400

    navigation routing

--- app.js
+++ app.js
@@ -6,13 +6,22 @@ var port = process.env.PORT || 5000;
 
 
 app.use(express.static('public'));
-app.set('views','./src/views');
-app.set('view engine','ejs');
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-app.get('/', function(req,res){
-    res.render('index',{title: 'hello from render',list: ['a','b']});
+app.get('/', function(req, res) {
+  res.render('index', {
+    title: 'hello from render',
+    nav: [{
+      Link: '/Books',
+      Text: 'Books'
+    }, {
+      Link: '/Authors',
+      Text: 'Authors'
+    }]
+  });
 });
 
-app.listen(port,function(err){
-   console.log('running server on port ' + port);
+app.listen(port, function(err) {
+  console.log('running server on port ' + port);
 });
