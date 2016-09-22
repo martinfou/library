801a575f3326d5095dd28799876a9e2eabaf4c35
martinfou <martinfou@gmail.com>
2016-09-21 21:42:03 -0400

    adding routes

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
-
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+bookRouter.route('/')
+  .get(function(req, res) {
+    res.send('Hello Books');
+  });
+
+bookRouter.route('/single')
+  .get(function(req, res) {
+    res.send('Hello single Books');
+  });
+
+app.use('/Books', bookRouter)
 
 app.get('/', function(req, res) {
   res.render('index', {
